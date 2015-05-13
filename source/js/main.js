148ae67527623704cf4fa76d112191a8f72c0d24
Liam Anderson <liam.anderson.91@gmail.com>
2015-05-13 19:11:43 +0100

    Start again.

--- source/js/main.js
+++
@@ -1,1 +0,0 @@
-var gui = require('nw.gui');
--- source/js/services.js
+++
@@ -1,7 +0,0 @@
-var webSql = angular.module('webSql', []);
-
-webSql.factory('serviceId', function() {
-  var shinyNewServiceInstance;
-  //factory function body that constructs shinyNewServiceInstance
-  return shinyNewServiceInstance;
-});
